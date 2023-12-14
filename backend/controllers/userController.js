4a32075872dd8f8aec39c44a0440d0df52197409
pamelagiselle8 <pamelaramirez@unitec.edu>
2023-12-14 17:04:33 -0600

    Crear usuario con Firebase

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,13 +2,104 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
 
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app");
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} = require("firebase/auth");
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHqlBtWHDSBncB39wKK_cBlodi4oOiO9M",
+  authDomain: "examenux-c9f86.firebaseapp.com",
+  projectId: "examenux-c9f86",
+  storageBucket: "examenux-c9f86.appspot.com",
+  messagingSenderId: "813028450508",
+  appId: "1:813028450508:web:d8fb6bd423467db8306269",
+  measurementId: "G-0G5NK2E3W3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // @desc Crear un usuario
-// @route GET /api/posts
-// @access Private
-const createUser = (req, res) => {
-  res.json({ message: "Create user" });
+// @route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      res.status(200).send({
+        msg: "Usuario creado exitosamente",
+        data: userCredential,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        msg: "No se pudo crear el usuario",
+        data: error.message,
+      });
+    });
+
+  // res.json({ message: "Create user" });
+};
+
+// @desc Login
+// @route POST /api/login
+// @access Public
+const loginUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      res.status(200).send({
+        msg: "Creado exitosamente",
+        data: userCredential,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        msg: "No se pudo crear el usuario",
+        data: error.message,
+      });
+    });
+  // res.json({ message: "Login user" });
+};
+
+// @desc Crear un usuario
+// @route POST /api/users
+// @access Public
+const logoutUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const auth = getAuth();
+  signOut(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      res.status(200).send({
+        msg: "Creado exitosamente",
+        data: userCredential,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        msg: "No se pudo crear el usuario",
+        data: error.message,
+      });
+    });
+  // res.json({ message: "Logout user" });
 };
 
 module.exports = {
-  createUser,
+  registerUser,
+  loginUser,
+  logoutUser,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/userController");
 
-router.post("/", createUser);
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
